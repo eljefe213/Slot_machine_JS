9e51092e3a6548bc4137696a07769afb2f50145c
Hamaz Abdelghani <101653284+eljefe213@users.noreply.github.com>
2023-06-02 11:49:50 +0200

    function numberOfLines et depositAmount

--- projet.js
+++ projet.js
@@ -5,3 +5,34 @@
 // 5. Check if the user won
 // 6. give the user their winnings
 // 7. play again
+
+const prompt = require("prompt-sync")();
+
+const deposit = () => {
+  while (true) {
+    const depositAmount = prompt("Enter a deposit amount: ");
+    const numberDepositAmount = parseFloat(depositAmount);
+
+    if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+      console.log("invalid deposit amount, please try again.");
+    } else {
+      return numberDepositAmount;
+    }
+  }
+};
+
+const getNumberOfLines = () => {
+  while (true) {
+    const Lines = prompt("Enter the number of lines to bet on(1-3): ");
+    const numberOfLines = parseFloat(Lines);
+
+    if (isNaN(numberOfLines) || (numberOfLines <= 0) | (numberOfLines > 3)) {
+      console.log("invalid number of lines, please try again.");
+    } else {
+      return numberOfLines;
+    }
+  }
+};
+
+const depositAmount = deposit();
+const numberOfLines = getNumberOfLines();
